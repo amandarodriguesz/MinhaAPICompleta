79e472725159a16720879fd575772a173ea354cc
Amanda Rodrigues <amanda.nrodrigues@hotmail.com>
2021-05-23 14:03:44 -0400

    Adicionar arquivos de projeto.

---
+++ src/DevIO.Api/DTO/FornecedorDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.DTO
+{
+    public class FornecedorDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
